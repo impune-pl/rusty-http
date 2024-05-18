acc1d90d27322b6573f49e632164b84e158fe6dc
Krzysztof Prorok <Krzysztof1397@gmail.com>
2024-05-18 19:34:05 +0200

    stage 1 with scrip adjusted for powershell

---
+++ your_server.ps1
@@ -0,0 +1,1 @@
+cargo run --quiet     --release     --target-dir=/tmp/codecrafters-http-server-target     --manifest-path D:\Dane\Programowanie\Rust\codecrafters\codecrafters-http-server-rust\Cargo.toml -- "$@"
