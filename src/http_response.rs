901e561f032f0f8b975ad202d3cae03779e8bcff
Krzysztof Prorok <Krzysztof1397@gmail.com>
2024-06-30 21:37:55 +0200

    handling files (very badly but still)

--- src/http_response.rs
+++ src/http_response.rs
@@ -18,8 +18,8 @@ pub fn generate_response(mut request: HttpRequest) -> String {
 fn make_response_from_file(http_protocol: &HttpProtocol, headers: Vec<String>, url: String) -> String {
     let url =  url.strip_prefix("/files").or(Some("/index.html")).map(String::from).unwrap();
     match get_file_contents(url) {
-        Ok(content) => make_response_string(HttpResponseCode::Ok200.to_string().as_str(), http_protocol, headers, Some(content)),
-        Err(_) => make_response_string("404 Not Found", http_protocol, vec![], None)
+        Ok(content) => make_response_file_string(HttpResponseCode::Ok200.to_string().as_str(), http_protocol, headers, Some(content)),
+        Err(_) => make_response_file_string("404 Not Found", http_protocol, vec![], None)
     }
 }
 
@@ -40,11 +40,24 @@ fn get_filesystem_path(url: String) -> Result<String, &'static str> {
     return Ok(base_path + parsed_url.path.clone().as_str().strip_prefix("/").unwrap());
 }
 
-fn make_response_string(response_code: &str, protocol: &HttpProtocol, mut headers: Vec<String>, body: Option<String>) -> String {
+fn make_response_file_string(response_code: &str, protocol: &HttpProtocol, mut headers: Vec<String>, body: Option<String>) -> String {
     match body {
         Some(body) => {
             headers.push(format!("Content-Length: {}", body.len()));
             headers.push("Content-Type: application/octet-stream".to_string());
+            let response = format!("{} {}\r\n{}\r\n\r\n{}", protocol, response_code, headers.join("\r\n"), body);
+            println!("Responding with:\r\n{}", response);
+            return response;
+        }
+        None => return format!("{} {}\r\n{}\r\n", protocol, response_code, headers.join("\r\n"))
+    }
+}
+
+fn make_response_string(response_code: &str, protocol: &HttpProtocol, mut headers: Vec<String>, body: Option<String>) -> String {
+    match body {
+        Some(body) => {
+            headers.push(format!("Content-Length: {}", body.len()));
+            headers.push("Content-Type: text/plain".to_string());
             let response = format!("{} {}\r\n{}\r\n\r\n{}", protocol, response_code, headers.join("\r\n"), body);
             println!("Responding with:\r\n{}", response);
             return response;
